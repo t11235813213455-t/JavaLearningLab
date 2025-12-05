8362cc6256acb7692aaf385150d053ad70a2c431
t11235813213455-t <t11235813213455@gmail.com>
2025-12-05 11:19:07 +0300

    remove HelloWorld and LeetCode

--- HelloWorld/src/sqrt/sqrt.java
+++
@@ -1,9 +0,0 @@
-package sqrt;
-
-public class sqrt {
-    public static double sqrt(double x) {
-        if (x < 0) {
-            throw new IllegalArgumentException("Expected non-negative number, got " + x);
-        } else return Math.sqrt(x);
-    }
-}
--- LeetCode/src/Main.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        int target = 2;
-        System.out.println(Solution.searchInsert(nums, target));
-    }
-}
